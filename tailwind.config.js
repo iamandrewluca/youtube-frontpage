60c458cbab9bcd6c047bf9148d5948d2188f4551
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-03-30 13:57:26 +0300

    chore: add tw plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,9 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/line-clamp'),
+	],
 };
