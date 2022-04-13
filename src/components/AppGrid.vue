40dd08e1dcc643191e967d788d9537a8077a6bd0
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-04-13 16:17:07 +0300

    Add image and avatar props

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -6,14 +6,37 @@ import AppVideo from './AppVideo.vue';
 	<div
 		class="p-4 grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-gray-50"
 	>
-		<AppVideo title="My video 1" img="https://picsum.photos/1920/1080" />
-		<AppVideo title="My video 2" img="https://picsum.photos/1920/1080" />
-		<AppVideo title="My video 3" img="https://picsum.photos/1920/1080" />
-		<AppVideo title="My video 3" img="https://picsum.photos/1920/1080" />
-		<AppVideo title="My video 3" img="https://picsum.photos/1920/1080" />
-		<AppVideo title="My video 4" img="https://picsum.photos/1920/1080" />
-		<AppVideo title="My video 5" img="https://picsum.photos/1920/1080" />
-		<AppVideo title="My video 6" img="https://picsum.photos/1920/1080" />
-		<AppVideo title="My video 7" img="https://picsum.photos/1920/1080" />
+		<AppVideo
+			image="https://picsum.photos/seed/10/1080/1920"
+			avatar="https://picsum.photos/seed/1/36/36"
+		/>
+		<AppVideo
+			image="https://picsum.photos/seed/11/1080/1920"
+			avatar="https://picsum.photos/seed/2/36/36"
+		/>
+		<AppVideo
+			image="https://picsum.photos/seed/12/1080/1920"
+			avatar="https://picsum.photos/seed/3/36/36"
+		/>
+		<AppVideo
+			image="https://picsum.photos/seed/13/1080/1920"
+			avatar="https://picsum.photos/seed/4/36/36"
+		/>
+		<AppVideo
+			image="https://picsum.photos/seed/14/1080/1920"
+			avatar="https://picsum.photos/seed/5/36/36"
+		/>
+		<AppVideo
+			image="https://picsum.photos/seed/15/1080/1920"
+			avatar="https://picsum.photos/seed/6/36/36"
+		/>
+		<AppVideo
+			image="https://picsum.photos/seed/16/1080/1920"
+			avatar="https://picsum.photos/seed/7/36/36"
+		/>
+		<AppVideo
+			image="https://picsum.photos/seed/17/1080/1920"
+			avatar="https://picsum.photos/seed/8/36/36"
+		/>
 	</div>
 </template>
--- src/components/AppVideo.vue
+++ src/components/AppVideo.vue
@@ -1,15 +1,19 @@
+<script setup>
+defineProps({
+	image: String,
+	avatar: String,
+});
+</script>
+
 <template>
 	<div>
 		<div class="relative">
-			<img
-				src="https://picsum.photos/1080/1920"
-				class="aspect-video object-cover"
-			/>
+			<img :src="image" class="aspect-video object-cover" />
 			<span class="video__time">14:10</span>
 		</div>
 
 		<div class="flex gap-3 items-start mt-2">
-			<img src="https://picsum.photos/36/36" class="rounded-full" />
+			<img :src="avatar" class="rounded-full" />
 			<div class="grow">
 				<h4 class="line-clamp-2 video__title">
 					<span class="video__subtitle">Playing Fortnite</span>
